14ed2d3dac6b2726d4da91a1edd67eac49875f79
shahmimajid <im.shahmi@gmail.com>
2022-10-03 23:45:47 +0800

    Conditional Rendering

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp(
     {
         data() {
             return {
+                showBooks: true,
                 title: 'The Final Empire',
                 author: 'Brandon Charlie',
                 age: 43
             }
         },
         methods: {
-            changeTitle(title) {
-                this.title = title
+            toggleShowBooks() {
+                this.showBooks = !this.showBooks
             }
         }
     }
