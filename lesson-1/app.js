8925f95d2d927dcbc6dc1e55ac460a81dc605f60
shahmimajid <im.shahmi@gmail.com>
2022-10-04 23:51:44 +0800

    Dynamic class - isFav

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -15,6 +15,10 @@ const app = Vue.createApp(
             toggleShowBooks() {
                 this.showBooks = !this.showBooks
             },
+            toggleFav(book) {
+                book.isFav = !book.isFav
+
+            },
             handleEvent(e, data) {
                 console.log(e, e.type)
                 if (data) {
